e63ddc608bde30370aefa10df3f84263bb6487d2
EdgardoDiBello <edgardo1906.edb@gmail.com>
2020-12-06 11:59:19 -0500

    Programming assignment 2 with the code up and running.

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,31 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  invertida <- NULL
+  set <- function(y) {
+    x <<- y
+    invertida <<- NULL
+  }
+  get <- function() x
+  setinvertida <- function(inver) invertida <<- inver
+  getinvertida <- function() invertida
+  list(set = set, get = get,
+       setinvertida = setinvertida,
+       getinvertida = getinvertida)
+  
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invertida <- x$getinvertida()
+  if(!is.null(invertida)) {
+    message("Se consigio la inversa de la matriz")
+    return(invertida)
+  }
+  data <- x$get()
+  invertida <- solve(data, ...)
+  x$setinvertida(invertida)
+  invertida
 }
